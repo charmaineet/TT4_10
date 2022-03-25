e8a7aa1b21cba180cb8e0b2e6c5359031e9ba041
Charmaine <charmainetmj@gmail.com>
2022-03-25 14:07:14 +0800

    added more api functions

--- test_api.py
+++ test_api.py
@@ -1,5 +1,6 @@
 from flask import Flask, request, jsonify
 from flaskext.mysql import MySQL
+import pymysql
 
 
 app = Flask(__name__)
@@ -14,9 +15,11 @@ curr = conn.cursor()
 
 
 @app.route('/customer', methods = ['GET'])
-def customer_all():
+def customer_get():
+    if len(request.args) > 1:
+        return jsonify({'message': 'sorry please input one query parameter at a time or the api will only fulfil the criteria of the last query parameter'})
 
-    parameter_list = ['customer_name', 'customer_phone', 'customer_address']
+    parameter_list = ['CustomerId', 'customer_name', 'customer_phone', 'customer_address', 'balance']
 
     for params in parameter_list:
         print(params)
@@ -32,7 +35,7 @@ def customer_all():
 
 
 @app.route('/loan', methods = ['GET'])
-def loan_all():
+def loan_get():
 
     parameter_list = ['loanId', 'loan_amount']
 
@@ -53,7 +56,7 @@ def loan_all():
 
 @app.route('/customer', methods = ['PUT'])
 def customer_put():
-    # module_code, module_title,module_instructor, module_credits = '', '', '', 0
+
     parameters = {
         'CustomerId': '\'None\'',
         'customer_name': '\'None\'',
@@ -67,14 +70,13 @@ def customer_put():
             parameters[parameter_names[param]] = request.args[parameter_names[param]]
         else:
             return f'{parameter_names[param]} not found'
+    try:
+        curr.execute('INSERT INTO customer VALUES (\'{0}\',\'{1}\',\'{2}\', \'{3}\', \'{4}\')'.format(parameters['CustomerId'],parameters['customer_name'], parameters['customer_phone'], parameters['customer_address'], parameters['balance']))
+        conn.commit()
+    except pymysql.err.IntegrityError:
+        return jsonify({'message': 'duplicate primary key'})
 
-
-
-    # print('INSERT INTO customer VALUES (\'{0}\',\'{1}\',\'{2}\', \'{3}\')'.format(parameters['customer_name'], parameters['customer_phone'], parameters['customer_address']))
-    curr.execute('INSERT INTO customer VALUES (\'{0}\',\'{1}\',\'{2}\', \'{3}\', \'{4}\')'.format(parameters['CustomerId'],parameters['customer_name'], parameters['customer_phone'], parameters['customer_address'], parameters['balance']))
-
-    conn.commit()
-    return 'Posted'
+    return jsonify({'message':'Posted'})
 
 
 @app.route('/loan', methods = ['PUT'])
@@ -90,14 +92,13 @@ def loan_put():
             parameters[parameter_names[param]] = request.args[parameter_names[param]]
         else:
             return f'{parameter_names[param]} not found'
+    try:
+        curr.execute('INSERT INTO loan VALUES (\'{0}\',\'{1}\')'.format(parameters['LoanId'],parameters['loan_amount']))
+        conn.commit()
+    except pymysql.err.IntegrityError:
+        return jsonify({'message': 'duplicate primary key'})
 
-
-
-    # print('INSERT INTO customer VALUES (\'{0}\',\'{1}\',\'{2}\', \'{3}\')'.format(parameters['customer_name'], parameters['customer_phone'], parameters['customer_address']))
-    curr.execute('INSERT INTO loan VALUES (\'{0}\',\'{1}\')'.format(parameters['LoanId'],parameters['loan_amount']))
-
-    conn.commit()
-    return 'Posted'
+    return jsonify({'message':'Posted'})
 
 
 @app.route('/customer/update_balance', methods=['PATCH'])
@@ -105,9 +106,7 @@ def loan_put():
 def customer_patch_balance():
 
     if 'CustomerId' in request.args:
-        # print('UPDATE modules SET module_instructor = \'{0}\' WHERE module_code = \'{1}\''.format(request.args['module_instructor'], request.args['module_code']))
         curr.execute('UPDATE customer SET balance = \'{0}\' WHERE CustomerId = \'{1}\''.format(request.args['balance'], request.args['CustomerId']))
-        # NOTE THAT IF THE SQL QUERY HAS NUMBERS THEN DO NOT USE the escape BACKSLASH
         curr.execute('SELECT * FROM customer WHERE CustomerId = \'{0}\''.format(request.args['CustomerId']))
         updated_modules = curr.fetchall()
         conn.commit()
@@ -125,9 +124,7 @@ def customer_patch_balance():
 def customer_patch_phone():
 
     if 'CustomerId' in request.args:
-        # print('UPDATE modules SET module_instructor = \'{0}\' WHERE module_code = \'{1}\''.format(request.args['module_instructor'], request.args['module_code']))
         curr.execute('UPDATE customer SET customer_phone = \'{0}\' WHERE CustomerId = \'{1}\''.format(request.args['customer_phone'], request.args['CustomerId']))
-        # NOTE THAT IF THE SQL QUERY HAS NUMBERS THEN DO NOT USE the escape BACKSLASH
         curr.execute('SELECT * FROM customer WHERE CustomerId = \'{0}\''.format(request.args['CustomerId']))
         updated_modules = curr.fetchall()
         conn.commit()
@@ -143,9 +140,9 @@ def customer_patch_phone():
 def customer_patch_address():
 
     if 'CustomerId' in request.args:
-        # print('UPDATE modules SET module_instructor = \'{0}\' WHERE module_code = \'{1}\''.format(request.args['module_instructor'], request.args['module_code']))
+
         curr.execute('UPDATE customer SET customer_address = \'{0}\' WHERE CustomerId = \'{1}\''.format(request.args['customer_address'], request.args['CustomerId']))
-        # NOTE THAT IF THE SQL QUERY HAS NUMBERS THEN DO NOT USE the escape BACKSLASH
+
         curr.execute('SELECT * FROM customer WHERE CustomerId = \'{0}\''.format(request.args['CustomerId']))
         updated_modules = curr.fetchall()
         conn.commit()
@@ -154,8 +151,51 @@ def customer_patch_address():
         if 'CustomerId' not in request.args:
             return 'Customer Id not sent'
         else:
-            return 'customer_address not sent'
+            return jsonify({'message': 'customer_address not sent'})
 
+@app.route('/loan/update_amount', methods=['PATCH'])
+def loan_patch_loan_amount():
+
+    if 'LoanId' in request.args:
+
+        curr.execute('UPDATE loan SET loan_amount = \'{0}\' WHERE LoanId = \'{1}\''.format(request.args['loan_amount'], request.args['LoanId']))
+
+        curr.execute('SELECT * FROM loan WHERE LoanId = \'{0}\''.format(request.args['LoanId']))
+        updated_modules = curr.fetchall()
+        conn.commit()
+        return 'Updated!' + str(updated_modules)
+    else:
+        if 'LoanId' not in request.args:
+            return jsonify({'message':'Loan Id not sent'})
+        else:
+            return jsonify({'message': 'loan_amount not sent'})
+
+
+
+
+@app.route('/customer/deletebyid', methods = ['DELETE'])
+def customer_delete():
+    if 'CustomerId' in request.args:
+
+        curr.execute('DELETE FROM customer WHERE CustomerId = \'{0}\''.format(request.args['CustomerId']))
+        conn.commit()
+        return jsonify({'message':'Deleted!'})
+
+
+    else:
+        return jsonify({'message':' no customer id provided'})
+
+@app.route('/loan/deletebyid', methods = ['DELETE'])
+def loan_delete():
+    if 'LoanId' in request.args:
+
+        curr.execute('DELETE FROM loan WHERE LoanId = \'{0}\''.format(request.args['LoanId']))
+        conn.commit()
+        return jsonify({'message':'Deleted!'})
+
+
+    else:
+        return jsonify({'message':' no customer id provided'})
 
 
 
