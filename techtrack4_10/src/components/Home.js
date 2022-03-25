627928354bcefc9bb857fc583665a90813b57ff0
Xiang Kai <xkleow.2018@smu.edu.sg>
2022-03-25 17:04:52 +0800

    Added API functions

--- techtrack4_10/src/components/Home.js
+++ techtrack4_10/src/components/Home.js
@@ -5,9 +5,14 @@ export default function Home() {
 
   const [balance, SetBalance] = React.useState("");
 
+  const [loan, SetLoan] = React.useState("");
+
+  const [loanHistory, SetLoanHistory] = React.useState([]);
+
   const [toggle1, setToggle1] = React.useState(true);
 
   const [toggle2, setToggle2] = React.useState(true);
+  
 
   function handleClick1() {
     setToggle1((prevtoggle1) => !prevtoggle1);
@@ -19,28 +24,42 @@ export default function Home() {
   React.useEffect(() => {
     fetch("http://ec2-52-221-194-162.ap-southeast-1.compute.amazonaws.com:5000/customer?CustomerId=8")
       .then((res) => res.json())
-      .then((data) => SetBalance(data.loan_amount));
-  }, []);
-  console.log(balance)
+      .then((data) => SetLoan(data));
+  }, []
+  );
 
+  React.useEffect(() => {
+    fetch("")
+      .then((res) => res.json())
+      .then((data) => SetBalance(data));
+  }, []
+  );
+
+  React.useEffect(() => {
+    fetch("")
+      .then((res) => res.json())
+      .then((data) => SetLoanHistory(data));
+  }, []
+  );
+
+  const History = loanHistory.map((d) => <li key={d.name}>{d.name}</li>);
   
-
   return (
     <div>
       <nav className="navbar">
         <h1>DBS</h1>
       </nav>
-      <span>Current Account Balance: </span>{" "}
-      {toggle1 ? <span>$123</span> : <span>***</span>}{" "}
+      <span>Current Account Balance: </span>
+      {toggle1 ? <span>{balance[1]}</span> : <span>***</span>}
       <button onClick={handleClick1}>Hide</button>
       <hr />
       <span>Current Loan: </span>
-      {toggle2 ? <span></span> : <span>***</span>}{" "}
+      {toggle2 ? <span>{loan[1]}</span> : <span>***</span>}
       <button onClick={handleClick2}>Hide</button>
       <hr />
       <h2>Loan History </h2>
       <button>Apply New Loan</button>
-      <ul>$123</ul>
+      {History}
     </div>
   );
 }
